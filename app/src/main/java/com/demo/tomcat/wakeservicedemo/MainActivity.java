725758296003427cff5c7e6365a2e94985d1ca6f
tomcat <tomcat.cheng@microlife.com.tw>
2018-04-24 17:32:04 +0800

    modify code.

--- app/src/main/java/com/demo/tomcat/wakeservicedemo/MainActivity.java
+++ app/src/main/java/com/demo/tomcat/wakeservicedemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.demo.tomcat.wakeservicedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// https://www.itstrike.cn/Question/dceae027-c3dd-4726-a6e1-ef84afc023f9.html
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
